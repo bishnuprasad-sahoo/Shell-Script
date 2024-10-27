a827502ba318156bb42ef54a5fd802c55172f36f
bishnudevops@gmail <bishnudevops@gmail.com>
2024-10-27 17:26:06 +0530

    fresh

--- nodehealth.sh
+++ nodehealth.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-##################
+#######################
+# Author: Bishnu
+# Date: 27/10/2024
 #
+# This script outputs the node health
 #
-#
-#
-##################
+# Version: v1
+#######################
+
+set -x #Debug mode
+
+df -h
+
+free -g
+
+nproc
